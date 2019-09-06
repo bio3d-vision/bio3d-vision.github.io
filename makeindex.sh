3f1e1840a3a690a671402b5b66e18fbea6ca4b9a
Matthew Guay <matt.d.guay@gmail.com>
2019-09-06 12:43:35 -0400

    Layout change attempt

--- makeindex.sh
+++ makeindex.sh
@@ -5,7 +5,6 @@ layout: page
 title: 'bio3d-vision'
 use-site-title: false
 ---
-<div class='col-lg-6>'
 " > indexhead.html
 
 echo "---
@@ -13,7 +12,6 @@ layout: page
 title: 'platelet-em'
 use-site-title: false
 ---
-<div class='col-lg-6>
 " > descripthead.html
 
 pandoc platelet-description.md -o description.html
@@ -21,13 +19,8 @@ pandoc platelet-description.md -o description.html
 pandoc README.md -o README.html
 
 cat indexhead.html README.html > index.html
-echo "
-</div>" >> index.html
 
 cat descripthead.html description.html > platelet-description.html
-echo "
-</div>" >> platelet-description.html
-
 
 rm indexhead.html
 
